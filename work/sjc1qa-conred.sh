157210972196bba279d00bd3643a01f4ad46d65d
Shawn <shawn.green@hobsons.com>
2016-05-20 16:42:14 -0400

    .

--- work/sjc1qa-conred.sh
+++ work/sjc1qa-conred.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ANSIHOST=sjc1qa-conred01
+ANSIHOST=sjc1qa-conred03
 
-/usr/bin/ansible-playbook sjc1qa-conred.yml --extra-vars "vmware_guest_name=$ANSIHOST"
-/usr/bin/ansible-playbook /etc/ansible/playbooks/bootstrap.yml -l $ANSIHOST
-/usr/bin/ansible-playbook /etc/ansible/playbooks/redis.yml -l $ANSIHOST
+#/usr/bin/ansible-playbook sjc1qa-conred.yml --extra-vars "vmware_guest_name=$ANSIHOST"
+#/usr/bin/ansible-playbook /etc/ansible/playbooks/bootstrap.yml -l $ANSIHOST
+/usr/bin/ansible-playbook /etc/ansible/playbooks/redis.yml -l $ANSIHOST -vv
